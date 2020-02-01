634298e6b515ae7d082598e8a5328844db1c98bf
Himanshu Saini <sainihimanshu.1999@gmail.com>
2020-02-01 11:13:42 +0530

    Second commit

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from bokeh.plotting import figure, output_file , show , ColumnDataSource
+from bokeh.plotting import figure, output_file , show , save,  ColumnDataSource
 import pandas
+from bokeh.models.tools import HoverTool
+from bokeh.transform import factor_cmap
+from bokeh.palettes import Reds8
+
 
 df  = pandas.read_csv('cars.csv')
 
@@ -24,9 +28,30 @@ p.hbar(
     right = 'Horsepower',
     left = 0,
     height = 0.5,
-    color = 'green',
-    fill_alpha = 0.5,
-    source = source
+    fill_color=factor_cmap(
+      'Car',
+      palette=Reds8,
+      factors=car_list
+    ),
+    fill_alpha = 0.9,
+    source = source,
+    legend = 'Car'
 )
 
-show(p)
+p.legend.orientation = 'vertical'
+p.legend.location = 'top_right'
+p.legend.label_text_font_size = '10px'
+
+hover = HoverTool()
+hover.tooltips = """
+    <div>
+        <h3>@Car</h3>
+        <div><strong>HP: </strong>@Horsepower</div>
+        <div><strong>Price: </strong>@Price</div>
+        <div> <img src = "@Image" alt = "" width = "200" /> </div> 
+    </div>
+"""
+
+p.add_tools(hover)
+
+save(p)
